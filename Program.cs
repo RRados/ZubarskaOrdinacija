b12562c2dcd42497eece5dcb30e8fb76bc11822c
Rados Rajcic <rale_pr@hotmail.com>
2022-06-26 00:03:45 +0200

    Izmena za prog.cs

--- Program.cs
+++ Program.cs
@@ -17,7 +17,6 @@ namespace ZubarskaOrdinacija
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new OsnovnaForma());
-            // savo
         }
     }
 }
