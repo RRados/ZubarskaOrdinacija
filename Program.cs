47c59f430d0e88abea074c7bb69a951675861389
Radoš <76565416+RRados@users.noreply.github.com>
2022-06-26 00:02:30 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace ZubarskaOrdinacija
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new OsnovnaForma());
+            // savo
         }
     }
 }
